d1dd97abeae223d26e30666b27cca963352e4d45
olah sandor <43461831+GSG9HUN@users.noreply.github.com>
2023-05-11 20:56:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ ageNet = cv2.dnn.readNet(ageConfig, ageWeights)
 # Életkor lista.
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
            '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-# ezek igazából fogalmam sincs hogy micsodák.
 modelMean = (78.4263377603, 87.7689143744, 114.895847746)
 
 Boxes = []  # Az arcok keretezésére.
